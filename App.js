a236c9923792f4766c6760e22ab7c992aaa8b0f0
Subrata Dutta <subratadutta@Subratas-MBP.Dlink>
2024-03-14 22:39:14 +0530

    Started with Heading Component

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React !!!");
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React !!!");
+
+
+const Title = () => <strong id="title" tabIndex="5" className="class"> This is Title Component inside Heading Component !!!!</strong>;
+
+const HeadingComponent = () => {
+    return (
+        <div className="container">
+            <Title/>
+            <p className="p-class">This is Functional component in p-tag....</p>
+            <h1 id="heading" tabIndex="5" className="class"> 
+    This is my first react functional component....</h1>
+        </div>
+    
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # React Start
+- Install react
+- Install react-dom
+
+# Set up is done using Parcel
+- Parcel is running the server
+- Parcel is doing HMR (Hot Module Replacement)
+- import react & react-dom
+- add module attribute to our App.js file
+- once module attribute added, now import can be used
+- Parcel is doing bundling, minification, optimization, caching, image optimization for React App
+
+# Parcel using Babel 
+- Parcel is using Babel to transpile JSX code
